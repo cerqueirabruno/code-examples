2a52651bfc96520c5de7ac5b66f9bd9677a919ca
cerqueirabruno <bocerqueira@hotmail.com>
2023-08-30 17:34:55 -0300

    atualizando repositório

--- js/dom1-introduction.js
+++ js/dom1-introduction.js
@@ -2,14 +2,21 @@
 
 // https://www.w3schools.com/jsref/obj_window.asp
 
+/* ========== ========== DEFINIÇÃO ========== ========== */
+
+// Document Object Model (Modelo de Objeto de Documento) em inglês. É uma representação estruturada de um documento HTML ou XML que permite que os programas acessem e manipulem o conteúdo, estrutura e estilo do documento de forma dinâmica. No contexto do JavaScript, o DOM é uma interface de programação que permite que scripts interajam com elementos HTML ou XML, alterando seu conteúdo, estilo e estrutura, bem como respondendo a eventos, como cliques de mouse e pressionamentos de tecla.
+
+window // é o elemento "topo da pirâmide" na árvore do DOM;
+window.document // é o HTML da página;
+
 /* ========== ========== SELETORES (CONSULTAS DOM) ========== ========== */
 
 document.getElementById('id'); // retorna apenas o elemento
 document.getElementsByClassName('classe'); // retorna HTMLCollection // informar o ìndice ou usar o "FOR"
 document.getElementsByTagName('tag'); // retorna HTMLCollection // informar o ìndice ou usar o "FOR"
 
-document.querySelector('seletor css'); // seletor universal // apenas 1 elemento // retorna o elemento
-document.querySelectorAll('seletor css'); // seletor univeral // todos elementos // retorna um NodeList
+document.querySelector('seletor css'); // seletor universal // retorno: apenas 1 elemento // retorna o elemento
+document.querySelectorAll('seletor css'); // seletor univeral // retorno: lista com todos elementos // retorna um NodeList
 
 
 /* ========== ========== PROPRIEDADES ========== ========== */
--- js/dom2-examples.js
+++ js/dom2-examples.js
@@ -6,6 +6,42 @@ let newElement2 = document.createElement('div');
 newElement1.innerText = 'Um texto sem marcação HTML';
 newElement2.innerHTML = '<b>Um texto com marcação HTML</b>';
 
+/* ========== ========== modificando 1 elemento ========== ========== */
+
+document.getElementsByClassName('teste')[0].innerText = 'testando';
+
+/* ========== ========== modificando todos elementos ========== ========== */
+
+let test = document.getElementsByClassName('teste2');
+
+for (let index = 0; index < test.length; index += 1) {
+    test[index].innerText = 'testando 123...';
+};
+
+/* ========== ========== HTMLCollection vs NodeList ========== ========== */
+
+// HTMLCollection contém apenas elementos de nó HTML, o NodeList pode conter qualquer tipo de nó;
+
+/* ========== ========== criar elementos ========== ========== */
+
+let arrayDeItens = ['item 1', 'item 2', 'item 3'];
+
+let selecionaHtml = document.querySelector('.classe');
+
+for (let index = 0; index < arrayDeItens.length; index += 1) {
+    let itens = arrayDeItens[index];
+
+    let criarLista = document.createElement('li');
+    criarLista.innerText = itens;
+    criarLista.className = 'itens-de-teste'
+
+    selecionaHtml.appendChild(criarLista);
+}
+
+/* ========== ========== removendo elemento ========== ========== */
+
+/* .includes */
+
 /* ========== ========== target / type ========== ========== */
 
 /* ========== ========== onload ========== ========== */
@@ -17,3 +53,5 @@ window.onload = jsCarregado;
 function jsCarregado() {
     alert("página carregada!")
 };
+
+/* ========== ========== @@@ ========== ========== */
