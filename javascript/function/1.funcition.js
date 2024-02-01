c992f5753535336bbddbefce9b36073db7e835c0
cerqueirabruno <bocerqueira@hotmail.com>
2024-02-01 15:52:32 -0300

    organizando repositório

--- javascript/function/1.funcition.js
+++ javascript/function/1.funcition.js
@@ -7,28 +7,28 @@ function nomeDaFuncao (parametro1, parametro2) {
 
 nomeDaFuncao('argumento1', 'argumento2'); // invoca a função;
 
-// função hoisting
+// função hoisting: é o comportamento padrão do JavaScript de mover todas as declarações de função para o topo do escopo atual;
 functionHoisting();
 
 function functionHoisting(params) {
   console.log('Hello World');
 }
 
-// function expression
+// function expression: é uma função que pode ser atribuída a uma variável;
 const functionExpression = function (params) {
   console.log('Hello World');
 }
 
 functionExpression();
 
-// arrow function
+// arrow function: é uma forma mais curta de escrever uma função;
 const arrowFunction = () => {
   console.log('Hello World');
 }
 
 arrowFunction();
 
-// objeto dentro de função
+// objeto dentro de função: é possível criar um objeto dentro de uma função;
 const myObject = {
   myFunction: function () {
     console.log('Hello World');
