5612a74c1f9bcddf47422727d00a72f53f498cef
cerqueirabruno <bocerqueira@hotmail.com>
2024-03-01 23:01:41 -0300

    teste

---
+++ sql/teste.sql
@@ -0,0 +1,27 @@
+/*
+
+DQL: Data Query Language
+  SELECT: Consulta de dados
+
+DML: Data Manipulation Language
+  INSERT: Inserção de dados
+  UPDATE: Atualização de dados
+  DELETE: Exclusão de dados
+
+DDL: Data Definition Language
+  CREATE: Criação de objetos
+  ALTER: Alteração de objetos
+  DROP: Exclusão de objetos
+
+DCL: Data Control Language
+  GRANT: Concessão de privilégios
+  REVOKE: Revogação de privilégios
+
+TCL: Transaction Control Language
+  BEGIN: 
+  COMMIT: Confirmação de transação
+  ROLLBACK: Desfazimento de transação
+  SAVEPOINT: Pontos de salvamento
+  SET TRANSACTION
+
+ */
