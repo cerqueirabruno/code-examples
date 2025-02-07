2f4d56f6c78cc78bb9f21fa47f87cbd844a9f3e7
cerqueirabruno <bocerqueira@hotmail.com>
2025-02-07 16:30:39 -0300

    add test

---
+++ test/mocha.js
@@ -0,0 +1,10 @@
+// TIPOS DE TESTE: UNITÁRIO, INTEGRAÇÃO E2E;
+// JEST:
+// MOCHA: freamework de testes.
+// CHAI: biblioteca de asserções.
+// SINON: biblioteca de mocks. ???
+// CHAI-HTTP: simular uma request a nossa API sem inicializá-la manualmente.
+
+// INSTALAÇÃO
+// npm install mocha@8.4.0 chai@4.3.4 --save-dev --save-exact
+// npm install chai-http@4.3.0 --save-dev --save-exact
